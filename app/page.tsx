4be9d7a564249c458764a5ad4905ace6f8a35e7d
Alexander Brgmnn <alxndr.wlfgng.brgmnn@gmail.com>
2025-05-03 22:56:48 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,18 @@ import { getGithubRepoData } from '@/utils/data';
 import Image from 'next/image';
 
 export default async function Home() {
-  const { name, description } = await getGithubRepoData();
   return (
     <main className='w-full py-12 md:py-24 lg:py-32 font-sans bg-gray-800 space-y-6'>
       <div className='container px-4 md:px-6 text-center mx-auto'>
         <h1 className='text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none text-white'>
-          {name}
+          OpenPlaceholder powered by evarioo
         </h1>
         <p className='mx-auto max-w-[700px] text-gray-300 md:text-xl mt-4'>
-          {description}
+          Use it for free. Now.
         </p>
-        <div className='flex items-center w-[120px] mx-auto mt-6'>
-          <p>Original from <a href="https://github.com/akshitkrnagpal/open-placeholder">akshitkrnagpal</a></p>
-        </div>
+        <p className='mx-auto max-w-[700px] text-gray-300 md:text-xl mt-4'>
+          Original from <a href="https://github.com/akshitkrnagpal/open-placeholder">akshitkrnagpal</a>
+        </p>
       </div>
       <Image
         priority
