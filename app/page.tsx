b944ad615ee3ffe4ff0e1a74ce79047be1ccf9b0
Alexander Brgmnn <alxndr.wlfgng.brgmnn@gmail.com>
2025-05-03 22:50:01 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default async function Home() {
           {description}
         </p>
         <div className='flex items-center w-[120px] mx-auto mt-6'>
-Lorem ipsum
+<p>Original from <a href="https://github.com/akshitkrnagpal/open-placeholder">akshitkrnagpal</a></p>
         </div>
       </div>
       <Image
