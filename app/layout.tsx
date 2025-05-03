6b691fad70529b7d2d3552f9efe62cfd9a57be57
Alexander Brgmnn <alxndr.wlfgng.brgmnn@gmail.com>
2025-05-03 22:57:22 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en' className={`${GeistSans.variable} ${GeistMono.variable}`}>
-      <body className='font-sans'>{children}</body>
+      <body className='bg-gray-800 font-sans'>{children}</body>
     </html>
   );
 }
